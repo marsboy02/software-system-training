7d0611c904e833746268866494521003b12ef932
Marsboy02 <marsboy0619@gmail.com>
2023-12-20 12:06:42 +0900

    fix: 주식 데이터 값을 row를 10개에서 25개로 변경

--- src/external/external.service.ts
+++ src/external/external.service.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 @Injectable()
 export class ExternalService {
   url: string =
-    'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=GS4AfFAtMpuTaa5EMX6Zgtbp1HiiFmFv3IwNocx9ajQ0k9pd8Atx47e4OxKjw%2FDPVL3LGsB0xwjSCSjxdLqJpg%3D%3D&numOfRows=10&pageNo=1&resultType=json';
+    'https://apis.data.go.kr/1160100/service/GetStockSecuritiesInfoService/getStockPriceInfo?serviceKey=GS4AfFAtMpuTaa5EMX6Zgtbp1HiiFmFv3IwNocx9ajQ0k9pd8Atx47e4OxKjw%2FDPVL3LGsB0xwjSCSjxdLqJpg%3D%3D&numOfRows=25&pageNo=1&resultType=json';
   async getStockData() {
     const response = await axios.get(this.url);
     const stockData = response.data.response.body.items.item;
